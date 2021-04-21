3c30d9fc7d11f9ea5fa45788889519e202a65e2f
Ngan Tran <tuyetngantran157@gmail.com>
2021-04-21 07:28:27 -0700

    Add leaflet routing update

--- src/containers/Map/Booking.js
+++ src/containers/Map/Booking.js
@@ -1,38 +1,54 @@
-import React from 'react';
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, withLeaflet } from 'react-leaflet';
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  useMap
+} from "react-leaflet";
+import * as L from "leaflet";
+import "leaflet-routing-machine";
+import "leaflet-control-geocoder";
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 
-import './Search.scss';
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
-const BookingMap = () => {
-    // // const bookingMap = () => {
-    //     var mymap = L.map('mapid').setView([50.27264, 7.26469], 13);
-    //         console.log(mymap);
-    //         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
-    //         var control = L.Routing.control({
-    //         waypoints: [
-    //             L.latLng(38.7436056, -9.2304153),
-    //             L.latLng(38.5334477, -0.1312811)
-    //         ],
-    //         router: new L.Routing.osrmv1({
-    //             language: 'en',
-    //             profile: 'car'
-    //         }),
-    //         geocoder: L.Control.Geocoder.nominatim({})
-    //         }).addTo(mymap);
-    //     // return mymap;
-    // // }
-      
-    const position = [51.505, -0.09];
-    return (
-        <MapContainer className="map" id="mapid" center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
-    )
+function MyComponent() {
+  const map = useMap();
+  L.Routing.control({
+    waypoints: [
+      L.latLng(38.7436056, -9.2304153),
+      L.latLng(38.5334477, -0.1312811),
+    ],
+    router: new L.Routing.osrmv1({
+      language: "en",
+      profile: "car",
+    }),
+    geocoder: L.Control.Geocoder.nominatim({}),
+  }).addTo(map);
 
+  return null;
 }
 
-export default BookingMap;
+const Booking = () => {
+  return (
+    <div>
+      <Header />
+      <MapContainer
+        style={{ height: "85vh" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MyComponent />
+      </MapContainer>
+      <Footer />
+    </div>
+  );
+};
+
+export default Booking;
